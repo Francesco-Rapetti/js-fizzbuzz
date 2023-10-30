102e583946c7fa5bbd6c6b2c541b41d9440db299
Francesco Rapetti <rapettif80@yahoo.it>
2023-10-30 17:12:25 +0100

    Implement UI style

--- js/main.js
+++ js/main.js
@@ -2,14 +2,24 @@ const container = document.getElementById('container');
 
 for (let i = 1; i <= 100; i++) {
     const newBlock = document.createElement('p');
+    newBlock.className = 'glass col-1 mx-2';
 
     if (i % 3 === 0 && i % 5 == 0) {
+        newBlock.classList.add('bg-danger');
+        newBlock.classList.add('text-black');
+        newBlock.classList.add('glass-opacity');
         newBlock.innerHTML = 'FizzBuzz';
     }
     else if (i % 3 === 0) {
+        newBlock.classList.add('bg-orange')
+        newBlock.classList.add('text-black');
+        newBlock.classList.add('glass-opacity');
         newBlock.innerHTML = 'Fizz';
     }
     else if (i % 5 === 0) {
+        newBlock.classList.add('bg-purple')
+        newBlock.classList.add('text-black');
+        newBlock.classList.add('glass-opacity');
         newBlock.innerHTML = 'Buzz';
     }
     else {
